003eecd6a55f9b13183bcf6d1c70a500225fc95d
Jacky Liang <jackyliang@berkeley.edu>
2015-09-20 17:18:40 -0700

    Successful compilation and import of BIDMach and BIDMat. Currently program takes forever to load data (haven't successfully loaded data yet)

--- app/src/main/scala/com/BIDMach/MainActivity.scala
+++ app/src/main/scala/com/BIDMach/MainActivity.scala
@@ -1,15 +1,37 @@
 package com.BIDMach
 
+//Scala
 import scala.language.postfixOps
+import scala.concurrent.ExecutionContext.Implicits.global
 
+//Android
 import android.os.Bundle
 import android.app.Activity
 
-import scala.concurrent.ExecutionContext.Implicits.global
+//Java
+import java.io.File
+
+//BIDMach
+import BIDMach.Learner
+import BIDMat.{FMat, SMat}
+import BIDMat.MatFunctions._
+import BIDMach.models.GLM
 
 class MainActivity extends Activity {
 
-  override def onCreate(savedInstanceState: Bundle) = {
-	println("Hello, world!")
-  }
+	val dataPath = "data/BIDMach_Data/"
+
+	override def onCreate(savedInstanceState: Bundle) = {
+		println("In MainActivity for BIDMach.")
+		/*
+		println("Loading Data..")
+		val a = loadSMat(dataPath + "docs.smat.lz4")
+		println("Loading Labels..")
+		val c = loadFMat(dataPath + "cats.fmat.lz4")
+		println("Creating GLM..")
+		val (mm, mopts) = GLM.learner(a, c, 1)
+		println("Starting Training..")
+		mm.train
+		println("Finished Training!")*/
+	}
 }
